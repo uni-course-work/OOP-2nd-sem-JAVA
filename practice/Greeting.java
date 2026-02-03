932b04c36e6630dda835ec36013513f3fd6293b5
Amor Zephyr <amorzephyr@gmail.com>
2026-02-03 22:01:30 +0500

    Create gitignore: and simple helloworld and Greeting Program

---
+++ practice/Greeting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.print("What's your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name + "!");
+    }
+}
