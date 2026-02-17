7e98adda639df2b4d1cd59256c2ea1c138d558d5
Amor Zephyr <amorzephyr@gmail.com>
2026-02-17 12:09:08 +0500

    Replace loop with escape sequences in Pyrmaid

--- Labs/Lab1/Pyramid.java
+++ Labs/Lab1/Pyramid.java
@@ -1,10 +1,5 @@
 public class Pyramid {
   public static void main(String[] args) {
-    String stars = "*****";
-    int num = stars.length();
-    for (int i = 0; i < num; i++) {
-      System.out.println(stars);
-      stars = stars.substring(0, num - (i + 1));
-    }
+    System.out.println("*****\n****\n***\n**\n*");
   }
 }
